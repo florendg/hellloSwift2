51bf60cee80d34dd28e60fb510ff6e4ac7ba478a
floren <florendg@gmail.com>
2022-12-27 07:05:02 +0100

    Initial Commit

---
+++ hellloSwift/hellloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hellloSwiftApp.swift
+//  hellloSwift
+//
+//  Created by Floren de Gier on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct hellloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
